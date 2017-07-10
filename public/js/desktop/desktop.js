9c7732b1b2f0fc5898d5fbd517b567338d6a125e
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-07-10 08:37:56 +0700

    ok

--- public/js/desktop/desktop.js
+++ public/js/desktop/desktop.js
@@ -35,22 +35,29 @@ $(document).ready(function() {
 	
 	var myinter2 = setInterval(home_thuonghieu, 3000);
 
-	$('#popup-desktop').popup({
-        blur:true,
-        transition: 'all 0.3s',
-        scrolllock: false,
-        background: false,
-        backgroundactive:false,
-        onclose: function() {setTimeout(showchat, 20000);}
-    });
+	// $('#popup-desktop').popup({
+ //        blur:true,
+ //        transition: 'all 0.3s',
+ //        scrolllock: false,
+ //        background: false,
+ //        backgroundactive:false,
+ //        onclose: function() {setTimeout(showchat, 20000);}
+ //    });
     var d = new Date();
     var gio = d.getHours();
     var phut = d.getMinutes();
     if((7<gio&&gio<22)||(gio==22&&phut<=30)||(gio==7&&phut>=30)){
         setTimeout(showchat, 15000);
     }
-
-
+    $(".popup_desktop_close").click(function(event) {
+        //$('#popup-desktop').css('display', 'none');
+        $('#popup-desktop').removeClass('active');
+        setTimeout(showchat, 20000);
+    });
+    window.setInterval(function(){
+        $("#popup-desktop >.content-popup>.linktv >.table1 tr td:nth-child(2) a").toggleClass('active');
+        $("#popup-desktop >.content-popup>.nhapsodienthoai button").toggleClass('active');
+    }, 1000);
 });
 function open_tuvan(){
 	window.open("http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en","_blank");
@@ -87,5 +94,6 @@ function home_thuonghieu(){
 
 
 function showchat() {
-    $('#popup-desktop').popup('show');
+    //$('#popup-desktop').popup('show');
+    $('#popup-desktop').addClass('active');
 }
